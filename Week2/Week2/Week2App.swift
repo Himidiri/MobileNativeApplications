58629ca2143270e9abfed5aef85e51b03ec5020c
Himidiri <himidirihimakanika@gmail.com>
2025-11-18 17:20:58 +0530

    Added Week2

---
+++ Week2/Week2/Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week2App.swift
+//  Week2
+//
+//  Created by Himidiri Himakanika on 2025-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
