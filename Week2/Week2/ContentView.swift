9a99314925a934152c203a953641a15787a3df3c
Himidiri <himidirihimakanika@gmail.com>
2025-11-19 15:27:51 +0530

    Added ToDo List

--- Week2/Week2/ContentView.swift
+++ Week2/Week2/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var task:[String] = []
+    @State private var newTask:String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Add a New Task", text: $newTask).textFieldStyle(RoundedBorderTextFieldStyle()).padding(10)
+                    Spacer()
+                    Button("Add") {
+                        let trimmedText = self.newTask.trimmingCharacters(in: .whitespacesAndNewlines)
+                        guard !trimmedText.isEmpty else { return }
+                        self.task.append(trimmedText)
+                        self.newTask = ""
+                    }.disabled(self.newTask.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty).buttonStyle(.borderedProminent)
+                }
+                    
+                if self.task.isEmpty {
+                    Text("You have No tasks").foregroundStyle(Color.secondary)
+                }else{
+                    List{
+                        ForEach(self.task, id: \.self) { item in
+                            Text(item)
+                        }
+                        
+                        .onDelete{index in
+                            self.task.remove(atOffsets: index)
+                        }
+                    }
+                }
+            }.navigationTitle("My To Do List")
         }
-        .padding()
     }
 }
 
