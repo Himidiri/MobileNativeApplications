5f4636ae60d97e561d69ff46adb64e42a6645ad9
Himidiri <himidirihimakanika@gmail.com>
2025-11-19 20:19:19 +0530

    Added Week 3

---
+++ Week3/Week3/Week3App.swift
@@ -0,0 +1,17 @@
+//
+//  Week3App.swift
+//  Week3
+//
+//  Created by Himidiri Himakanika on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct Week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
