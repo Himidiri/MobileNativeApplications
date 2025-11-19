1f794d555b987eebac2f23fda6eddf1362656027
Himidiri <himidirihimakanika@gmail.com>
2025-11-19 23:14:16 +0530

    Added BookApp

---
+++ BookApp/BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Himidiri Himakanika on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
